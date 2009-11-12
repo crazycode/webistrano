cb9b431af6bb5c8524e9192bc3ffa8dd64368959
Ben Koski <bkoski@nytimes.com>
2009-11-12 14:28:17 -0500

    added base task and fixed typos

--- lib/webistrano/template/plain.rb
+++ lib/webistrano/template/plain.rb
@@ -6,12 +6,12 @@ module Webistrano
         :scm => 'subversion',
         :deploy_via => ':checkout',
         :scm_username => 'webistrano',
-        :scm_password => 'webist4no',
+        :scm_password => 'webistr4no',
         :user => 'root',
         :web_user => 'www-data',
         :writable_directories => 'tmp',
         :use_sudo => 'true',
-        :ssh_keys => '/var/local/keys/id-server-farm-test',
+        :ssh_keys => '/var/local/keys/id-test-server-farm',
         :repository => 'https://newsdev.ec2.nytimes.com/svn/projects/your-app-name',
         :target_rails_env => 'production'
       }.freeze
@@ -20,7 +20,20 @@ module Webistrano
         Plain project type: specific deploy configs should be added as recipies to each stage.
       EOS
       
-      TASKS = ""
+      TASKS =  <<-'EOS'
+        # allocate a pty by default as some systems have problems without
+        default_run_options[:pty] = true
+      
+        # set Net::SSH ssh options through normal variables
+        # at the moment only one SSH key is supported as arrays are not
+        # parsed correctly by Webistrano::Deployer.type_cast (they end up as strings)
+        [:ssh_port, :ssh_keys].each do |ssh_opt|
+          if exists? ssh_opt
+            logger.important("SSH options: setting #{ssh_opt} to: #{fetch(ssh_opt)}")
+            ssh_options[ssh_opt.to_s.gsub(/ssh_/, '').to_sym] = fetch(ssh_opt)
+          end
+        end
+      EOS
     end
   end
 end
